b604fffe8be1feb385e0684e99fd8adc7dedc8cc
vinod <vinodsinghrawat1905@gmail.com>
2020-09-06 19:05:35 +0530

    1st commit

---
+++ spring-Boot-Project/src/main/java/com/technocracy/spring/Boot/Project/SpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.technocracy.spring.Boot.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectApplication.class, args);
+	}
+
+}
